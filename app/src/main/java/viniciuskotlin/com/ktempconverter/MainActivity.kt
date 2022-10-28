e699ea10c46eeab0ef7f66e94ec3233524fd1261
unidesk <vinicius.nogueira@unidesk.com.br>
2022-10-28 10:22:38 -0300

    Adicionando lógica

--- app/src/main/java/viniciuskotlin/com/ktempconverter/MainActivity.kt
+++ app/src/main/java/viniciuskotlin/com/ktempconverter/MainActivity.kt
@@ -2,16 +2,17 @@ package viniciuskotlin.com.ktempconverter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.RadioButton
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var editText : EditText
-    lateinit var celciusRadio : RadioButton
-    lateinit var fahreinheitRadio : RadioButton
-    lateinit var converterButton : Button
+    lateinit var editText: EditText
+    lateinit var celciusRadio: RadioButton
+    lateinit var fahreinheitRadio: RadioButton
+    lateinit var converterButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,5 +24,18 @@ class MainActivity : AppCompatActivity() {
         fahreinheitRadio = findViewById(R.id.activitymain_fahreinheitradio) as RadioButton
 
         converterButton = findViewById(R.id.activitymain_converterButton) as Button
+        converterButton.setOnClickListener { converter(it) }
+    }
+
+    fun converter(view: View) {
+        var temp: Double = editText.text.toString().toDouble()
+
+        if (celciusRadio.isChecked) {
+            temp = (temp - 32) * 5 / 9
+        } else if (fahreinheitRadio.isChecked) {
+            temp = temp * 9 / 5 + 32
+        }
+
+        editText.setText(temp.toString())
     }
 }
